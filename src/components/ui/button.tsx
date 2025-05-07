9f1be3304042dc5a2add127b1cc8e50fb08ab336
mateussf99 <mateus.ms65@gmail.com>
2025-05-07 14:04:24 -0300

    ajuste do shadow do butto

--- src/components/ui/button.tsx
+++ src/components/ui/button.tsx
@@ -14,7 +14,7 @@ const buttonVariants = cva(
         destructive:
           "bg-destructive text-white shadow-xs hover:bg-destructive/90 focus-visible:ring-destructive/20 dark:focus-visible:ring-destructive/40 dark:bg-destructive/60",
         outline:
-          " text-primary bg-white shadow-xs hover:bg-primary hover:text-white ",
+          " text-primary bg-white hover:bg-primary hover:text-white ",
         secondary:
           "bg-secondary text-secondary-foreground shadow-xs hover:bg-secondary/80",
         ghost:
