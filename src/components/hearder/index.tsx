9bc7d8446f07edba0ff9e1e1379e4ce3b3af084c
mateussf99 <mateus.ms65@gmail.com>
2025-05-07 10:30:30 -0300

    ajustando a resposividade

--- src/components/hearder/index.tsx
+++ src/components/hearder/index.tsx
@@ -5,44 +5,51 @@ import Logo from '../../assets/Logo.png';
 function Index() {
   const location = useLocation();
 
+  const header = (
+    <>
+        <Link to="/">
+            <Button variant={location.pathname === '/' ? 'default' : 'outline'}>
+            Inicio
+            </Button>
+        </Link>
+        <Link to="/quem-somos">
+            <Button variant={location.pathname === '/quem-somos' ? 'default' : 'outline'}>
+            Quem somos
+            </Button>
+        </Link>
+        <Link to="/cliente">
+            <Button variant={location.pathname === '/cliente' ? 'default' : 'outline'}>
+            Quero ser cliente
+            </Button>
+        </Link>
+        <Link to="/parceiro">
+            <Button variant={location.pathname === '/parceiro' ? 'default' : 'outline'}>
+            Quero ser parceiro
+            </Button>
+        </Link>
+        <Link to="/parceiros">
+            <Button variant={location.pathname === '/parceiros' ? 'default' : 'outline'}>
+            Parceiros
+            </Button>
+        </Link>
+        <Link to="/login">
+            <Button variant={location.pathname === '/login' ? 'default' : 'outline'}>
+            Login
+            </Button>
+        </Link>
+    </>
+  );
+
+
   return (
     <div className="flex justify-between items-center">
       <div className="flex items-center gap-2">
         <img className="h-10 w-10 object-contain" src={Logo} alt="" />
         <h1 className="font-bold text-2xl text-primary">cisum club</h1>
       </div>
-      <div className="gap-4 flex items-center">
-        <Link to="/">
-          <Button variant={location.pathname === '/' ? 'default' : 'outline'}>
-            Inicio
-          </Button>
-        </Link>
-        <Link to="/quem-somos">
-          <Button variant={location.pathname === '/quem-somos' ? 'default' : 'outline'}>
-            Quem somos
-          </Button>
-        </Link>
-        <Link to="/cliente">
-          <Button variant={location.pathname === '/cliente' ? 'default' : 'outline'}>
-            Quero ser cliente
-          </Button>
-        </Link>
-        <Link to="/parceiro">
-          <Button variant={location.pathname === '/parceiro' ? 'default' : 'outline'}>
-            Quero ser parceiro
-          </Button>
-        </Link>
-        <Link to="/parceiros">
-          <Button variant={location.pathname === '/parceiros' ? 'default' : 'outline'}>
-            Parceiros
-          </Button>
-        </Link>
-        <Link to="/login">
-          <Button variant={location.pathname === '/login' ? 'default' : 'outline'}>
-            Login
-          </Button>
-        </Link>
-      </div>
+        <div className="gap-4 p-1 flex items-center">
+            {header}
+        </div>
     </div>
   );
 }
