7811de7f3d61dcd88bfc33608e54b3acfe41bfcc
mateussf99 <mateus.ms65@gmail.com>
2025-05-08 12:33:13 -0300

    ajuste no codigo

--- src/pages/home/index.tsx
+++ src/pages/home/index.tsx
@@ -12,8 +12,12 @@ const index = () => {
       </div>
       <div className='flex md:mt-14 p-1 items-center h-screen justify-center md:p-4  md:justify-between md:w-full md:h-screen'>
         <div className='flex p-10 gap-4 bg-white rounded-[10px] h-1/2 w-[400px] flex-col  items-center justify-center md:w-1/2 md:h-screen '>
-          <h1 className='text-3xl md:text-5xl text-primary font-bold '>Benefícios Exclusivos para Todos, em um Só Lugar.</h1>
-          <p className='md:text-3xl mt-5'>Acesse um clube que conecta empresas e clientes, oferecendo benefícios exclusivos e personalizados de forma simples e prática.</p>
+          <h1 className='text-3xl md:text-5xl text-primary font-bold '>
+            Benefícios Exclusivos para Todos, em um Só Lugar.
+          </h1>
+          <p className='md:text-3xl mt-5'>
+            Acesse um clube que conecta empresas e clientes, oferecendo benefícios exclusivos e personalizados de forma simples e prática.
+          </p>
           <Button variant="default" className='md:text-xl h-11'>Empresas Paceiras</Button>
         </div>
         <div className='hidden md:flex'>
@@ -21,7 +25,9 @@ const index = () => {
         </div>  
       </div>
       <div className='flex flex-col w-full h-screen items-center'>
-        <h1 className='text-primary font-bold text-5xl'>Nossos Parceiros</h1>
+        <h1 className='text-primary font-bold text-5xl'>
+          Nossos Parceiros
+        </h1>
         <div></div>
       </div>
 
