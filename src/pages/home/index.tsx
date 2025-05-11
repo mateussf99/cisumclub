a7ac73d93c2627b7f29cede121b0787d9871823f
mateussf99 <mateus.ms65@gmail.com>
2025-05-11 18:58:12 -0300

    tela 1 pronta falta so o footer

--- src/pages/home/index.tsx
+++ src/pages/home/index.tsx
@@ -15,12 +15,12 @@ import Apoiador8 from "../../assets/apoiador8.png";
 const index = () => {
   return (
     
-    <div className='flex flex-col w-full h-full justify-center bg-primary md:bg-white'>
+    <div className='flex flex-col w-full h-full justify-center bg-primary lg:bg-white'>
       <div className='fixed top-0 left-0 w-full z-10 '>
         <Header />
       </div>
-      <section className='flex md:mt-14 p-1 items-center h-screen justify-center md:p-4  md:justify-between md:w-full md:h-screen'>
-        <div className='flex p-10 gap-4 mt-4 bg-white rounded-[10px] h-[450px] w-[400px] flex-col  items-center justify-center md:w-1/2 md:h-screen '>
+      <div className='flex md:mt-14 p-1 items-center h-screen justify-center md:p-4  lg:justify-between md:w-full md:h-screen'>
+        <div className='flex p-10 gap-4 mt-4 bg-white rounded-[10px] h-[450px] w-[350px] flex-col  items-center justify-center lg:w-1/2 md:h-screen '>
           <h1 className='text-3xl md:text-5xl text-primary font-bold '>
             Benefícios Exclusivos para Todos, em um Só Lugar.
           </h1>
@@ -29,17 +29,17 @@ const index = () => {
           </p>
           <Button variant="default" className='md:text-xl h-11'>Empresas Paceiras</Button>
         </div>
-        <div className='hidden md:flex'>
+        <div className='hidden lg:flex'>
           <img src={Image1} alt="imagem que mostra pesoas conectadas"/>
         </div>  
-      </section>
-      <section className=' bg bg-primary mb-10 flex flex-col w-full h-full md:h-screen items-center justify-center'>
+      </div>
+      <div className=' bg-primary mb-10 flex flex-col w-full h-full md:h-screen items-center justify-center'>
 
-        <div className='flex flex-col m-4  md:h-[700px] md:w-[900px] rounded-2xl  items-center  '>
+        <div className='flex flex-col   lg:h-[700px] lg:w-[900px] rounded-2xl  items-center  '>
           <h1 className='text-white font-bold text-3xl md:text-5xl mt-10'>
             Nossos Apoiadores
           </h1>
-          <div className='grid grid-cols-2 md:grid-cols-4 gap-4 mt-10'>
+          <div className='grid grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-2 mt-10'>
             <div className=' h-35 w-40 bg-white rounded-[10px] flex items-center justify-center md:h-55 md:w-55'>
               <img src={Apoiador1} alt=""/>
             </div>
@@ -66,28 +66,82 @@ const index = () => {
             </div>
           </div>
         </div>
-      </section>
-      <section className='flex flex-col h-screen w-full bg-white items-center md:flex-row '>
-
-        <div className='flex flex-col m-4  items-center  '>
-          <h1 className='text-primary font-bold text-2xl md:text-5xl mt-10 mb-4'>
+      </div>
+      <div className='flex flex-col h-full md:h-screen w-full bg-white items-center md:flex-row '>
+        <div className='flex flex-col   items-center  '>
+          <h1 className='text-primary font-bold text-xl lg:text-5xl mt-10 mb-4'>
             Estratégia de marketing
           </h1>
-          <p className='text-justify p-10 pt-0 md:text-2xl'>
+          <p className='text-justify p-10 pt-0 md:text-xl'>
             Buscamos empresas parceiras comprometidas em oferecer descontos exclusivos aos nossos membros. Para os clientes, conseguem obter benefícios relevantes que agregam valor à sua experiência no clube. Já para as empresas parceiras, adquirem maior visibilidade, atração de novos clientes e fortalecimento da marca.
           </p>
         </div>
-
-        <div className='flex flex-col  m-4  items-center  '>
-          <h1 className='text-primary font-bold text-2xl md:text-5xl mt-10 mb-4'>
+        <div className='flex flex-col  items-center  '>
+          <h1 className='text-primary font-bold text-xl lg:text-5xl mt-10 mb-4'>
             Clubes de Benefícios
           </h1>
-          <p className='text-justify p-10 pt-0 md:text-2xl'>
+          <p className='text-justify p-10 pt-0 md:text-xl'>
             O clube de benefícios conecta clientes a descontos exclusivos em empresas parceiras, criando uma rede vantajosa para todos. Os membros do clube aproveitam vantagens exclusivas que agregam valor ao seu dia a dia, enquanto as empresas parceiras aumentam sua visibilidade, atraem um público qualificado e fortalecem sua reputação no mercado, ampliando suas oportunidades de crescimento.
           </p>
         </div>
-
-      </section>
+      </div>
+      <div className='flex-row  h-screen w-full bg-white justify-center md:flex-col'>
+        <div className='flex flex-col m-4  items-center  '>
+          <h1 className='text-primary mt-7 font-bold text-2xl md:text-5xl '>
+            Como o Cisum Club Funciona
+          </h1>
+          <div className='flex-col items-center grid md:grid-cols-3 rounded-2xl '>
+            <div className='flex-col border border-primary m-4  items-center rounded-2xl md:h-[210px]  '>
+              <h1 className='text-primary pt-0 pb-0 p-10 font-bold md:text-[15px] lg:text-[20px] mt-10 mb-4'>
+                01 CADASTRO
+              </h1>
+              <p className='text-justify p-10 pt-0 md:text-[10px] lg:text-[15px] '>
+                Negociamos benefícios e  descontos com empresas para clientes que forem membros do clube.
+              </p>
+            </div>
+            <div className='flex-col border border-primary m-4  items-center rounded-2xl md:h-[210px]  '>
+              <h1 className='text-primary pt-0 pb-0 p-10 font-bold md:text-[15px] lg:text-[20px] mt-5 mb-4'>
+                02 MÍDIAS
+              </h1>
+              <p className='text-justify p-10 pt-0 md:text-[10px] lg:text-[15px]'>
+                A empresa será destacada no site do CISUM Club e em nossa redes sociais, por meio de marketing de conteúdo, como objetivo de atrair novos clientes
+              </p>
+            </div>
+            <div className='flex-col border border-primary m-4  items-center rounded-2xl md:h-[210px]  '>
+              <h1 className='text-primary pt-0 pb-0 p-10 font-bold md:text-[15px] lg:text-[20px] mt-5 mb-4'>
+                03 CUSTO
+              </h1>
+              <p className='text-justify p-10 pt-0 md:text-[10px] lg:text-[15px]'>
+                O único custo para empresa é o benefício ou desconto que ela oferece. E será efetivado somente quando o cliente optar por utilizar o serviço da empresa, identificando-se como membro do CISUM Club
+              </p>
+            </div>
+            <div className='flex-col border border-primary m-4  items-center rounded-2xl md:h-[210px]  '>
+              <h1 className='text-primary pt-0 pb-0 p-10 font-bold md:text-[15px] lg:text-[20px] mt-5 mb-4'>
+                04 PROCEDIMENTO DO CLIENTE
+              </h1>
+              <p className='text-justify p-10 pt-0 md:text-[10px] lg:text-[15px]'>
+                O cliente adquire um acesso mensal, anualou vitalício e, deve apresentar um documento de indentificação com foto e CPF à empresa, e a empresa após verifica a validade do cadastro concede o benefício. 
+              </p>
+            </div>
+            <div className='flex-col border border-primary m-4  items-center rounded-2xl md:h-[210px] '>
+              <h1 className='text-primary pt-0 pb-0 p-10 font-bold md:text-[15px] lg:text-[20px] mt-5 mb-4'>
+                05 NETWORKING CRUZADO
+              </h1>
+              <p className='text-justify p-10 pt-0 md:text-[10px] lg:text-[15px]'>
+                O cliente explora serviços no site do clube e, ao encontrar uma empresa, pode descobrir outros serviços. Assim, aempresa não só atende o cliente inicial, mas tambem fica em evidência pela busca de clientes de outras empresas parceiras.
+              </p>
+            </div>
+            <div className='flex-col border border-primary m-4  items-center rounded-2xl md:h-[210px] '>
+              <h1 className='text-primary pt-0 pb-0 p-10 font-bold md:text-[15px] lg:text-[20px] mt-5 mb-4'>
+                06 COMO TORNAR PARCEIRO?
+              </h1>
+              <p className='text-justify p-10 pt-0 md:text-[10px] lg:text-[15px]'>
+                Você pode informar qual benefício ou desconto oferecer, ou nos contatar para definirmos juntos a meçhor proposta para nossas clientes.
+              </p>
+            </div>
+          </div>
+        </div>
+      </div>
     </div>
   )
 }
