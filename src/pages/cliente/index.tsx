caa55b39c05b1f688e912068f395a688e8b49906
mateussf99 <mateus.ms65@gmail.com>
2025-05-17 10:22:48 -0300

    ajustes de pois na tela de cliente

--- src/pages/cliente/index.tsx
+++ src/pages/cliente/index.tsx
@@ -4,58 +4,54 @@ import { Button } from "@/components/ui/button";
 function index() {
   return (
     <div className='flex flex-col w-full h-full justify-center bg-white'>
-      <div className='bg-primary pb-2 flex-col w-full h-full items-center '>
-        <div className='flex justify-center'>
-          <h1 className='text-2xl text-white font-bold mb-5 mt-5 md:text-4xl'>
+      <div className=' bg-primary  flex-col w-full md:h-screen justify-items-center items-center '>
+        <h1 className='text-2xl text-white font-bold mb-5 mt-5 md:text-4xl'>
           Nossos Planos
         </h1>
-        </div>
-        
-        <div className=' bg-primary  flex-col w-full h-screen justify-center items-center '>
-          <div className='flex flex-col md:flex-row  gap-5 w-full h-full justify-center items-center md:items-stretch'>
-            <div className='bg-white w-[200px] h-[300px] rounded-lg shadow-lg flex flex-col justify-center items-center md:h-[250px] md:mt-7'>
-              <h1 className='text-[15px] text-primary font-bold '>
-                Plano Básico
-              </h1>
-              <p className='font-bold text-4xl'>
-                <span className='text-[10px]'>R$</span>29,90
-              </p>
-              <p className='text-[10px]  mb-5'>
-                Por mês!
-              </p>
-              <Button variant="default">Eu Quero</Button>
-            </div>
-            
-
-            <div className='bg-white w-[200px] h-[300px]  rounded-lg shadow-lg flex flex-col justify-center items-center'>
-              <h1 className='text-[15px] text-primary font-bold '>
-                Plano Vitalício
-              </h1>
-              <p className='font-bold text-xl'>
-                Preço a consultar*.
-              </p>
-              <p className='text-[10px]  mb-5'>
-                Pagamento único a partir de US$ 10,00
-              </p>
-              <Button variant="default" >Lista de espera</Button>
-            </div>
-
-            <div className='bg-white w-[200px] h-[300px] rounded-lg shadow-lg flex flex-col justify-center items-center md:h-[250px] md:mt-7'>
-              <h1 className='text-[15px] text-primary font-bold '>
-                Plano Anual
-              </h1>
-              <p className='font-bold text-4xl'>
-                <span className='text-[10px]'>R$</span>249,90
-              </p>
-              <p className='text-[10px]  mb-5'>
-                Por Ano!
-              </p>
-              <Button variant="default">Eu Quero</Button>
-            </div>
-
+        <div className='flex flex-col md:flex-row  gap-5 w-full h-full justify-center items-center '>
+          <div className='bg-white w-[200px] h-[300px] rounded-lg shadow-lg flex flex-col justify-center items-center md:h-[250px] md:mt-7'>
+            <h1 className='text-[15px] text-primary font-bold '>
+              Plano Básico
+            </h1>
+            <p className='font-bold text-4xl'>
+              <span className='text-[10px]'>R$</span>29,90
+            </p>
+            <p className='text-[10px]  mb-5'>
+              Por mês!
+            </p>
+            <Button variant="default">Eu Quero</Button>
           </div>
+          
+
+          <div className='bg-white w-[200px] h-[300px]  rounded-lg shadow-lg flex flex-col justify-center items-center'>
+            <h1 className='text-[15px] text-primary font-bold '>
+              Plano Vitalício
+            </h1>
+            <p className='font-bold text-xl'>
+              Preço a consultar*.
+            </p>
+            <p className='text-[10px]  mb-5'>
+              Pagamento único a partir de US$ 10,00
+            </p>
+            <Button variant="default" >Lista de espera</Button>
+          </div>
+
+          <div className='bg-white w-[200px] h-[300px] rounded-lg shadow-lg flex flex-col justify-center items-center md:h-[250px] md:mt-7'>
+            <h1 className='text-[15px] text-primary font-bold '>
+              Plano Anual
+            </h1>
+            <p className='font-bold text-4xl'>
+              <span className='text-[10px]'>R$</span>249,90
+            </p>
+            <p className='text-[10px]  mb-5'>
+              Por Ano!
+            </p>
+            <Button variant="default">Eu Quero</Button>
+          </div>
+
         </div>
       </div>
+      
       <div className='bg-white pb-10 mt-10 flex flex-col w-full h-screen gap-4  items-center md:flex-row'>
         <div className='flex-col justify-items-center items-center md:w-1/2'>
           <h1 className='text-xl font-bold'>
@@ -70,7 +66,7 @@ function index() {
               A tabela ao lado mostra como funciona:
             </p>
           </div>
-          <Button variant="default">Lista de espera</Button>
+          <Button variant="default" className="md:text-xl">Lista de espera</Button>
           
         </div>
       <div className='flex-col mt-4 md:w-1/2 justify-items-center'>
--- src/pages/quem_somos/index.tsx
+++ src/pages/quem_somos/index.tsx
@@ -1,4 +1,4 @@
-import Header from '../../components/hearder'
+
 
 function index() {
   return (
