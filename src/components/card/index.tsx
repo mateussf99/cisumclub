a9a54dc77503e9f3a6d093d58ecd4a1d703a761b
mateussf99 <mateus.ms65@gmail.com>
2025-06-25 09:13:07 -0300

    ajuste de imagem

--- src/components/card/index.tsx
+++ src/components/card/index.tsx
@@ -236,7 +236,7 @@ function index(props: CardProps) {
             <img 
               src={props.associateImagemUrl}
               alt={`Logo ${props.title}`}
-              style= "border-radius: 10px;"
+              style={{ borderRadius: "10px" }}
               className="h-24 w-24 md:h-48 md:w-48 object-contain"
               onError={() => {
                 setImageError(true);
